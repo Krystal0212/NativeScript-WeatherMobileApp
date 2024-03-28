c973ee0f383029d75e60cd3a0b1680f7a10216b4
HaoNguyen9191 <thaingoc924282@gmail.com>
2024-03-29 01:56:33 +0700

    get latitude and longitude from user's location

--- app/main-view-model.js
+++ app/main-view-model.js
@@ -1,25 +1,58 @@
-import { Observable } from '@nativescript/core';
+import { Observable,CoreTypes } from '@nativescript/core';
+import * as geolocation from '@nativescript/geolocation';
 
-function getMessage(counter) {
-  if (counter <= 0) {
-    return 'Hoorraaay! You unlocked the NativeScript clicker achievement!';
-  } else {
-    return `${counter} taps left`;
+
+export class HomeViewModel extends Observable {
+  constructor() {
+    super();
+    this.weatherInfo = '';
   }
-}
 
-export function createViewModel() {
-  const viewModel = new Observable();
-  viewModel.counter = 42;
-  viewModel.message = getMessage(viewModel.counter);
+  convertUnixTimestamp(timestamp) {
+    var date = new Date(timestamp * 1000);
+    return date.toLocaleString(); // Converts to local time in a readable format
+  }
 
-  viewModel.onTap = () => {
-    viewModel.counter--;
-    viewModel.set('message', getMessage(viewModel.counter));
+  onGetWeather() {
+    geolocation.enableLocationRequest()
+    .then(() => {
+      geolocation.getCurrentLocation({ 
+        desiredAccuracy: CoreTypes.Accuracy.high,
+        maximumAge: 5000, 
+        timeout: 20000 })
+      .then((data)=>{
+        return {latitude: data.latitude,longitude: data.longitude}
+      })
+      .then(location=>{
 
-    // log the message to the console
-    console.log(getMessage(viewModel.counter));
-  };
-
-  return viewModel;
+        const lat = location.latitude; // Latitude
+        const lon = location.longitude	; // Longitude
+        const apiKey = '672346e591b20d5d0b1e99dc21df440e'; // API Key
+        
+        fetch(
+          `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            // Example: Extracting current temperature
+            const temperature = data.current
+              ? (data.current.temp - 273.15).toFixed(2)
+              : 'N/A';
+            const currentDateTime = this.convertUnixTimestamp(data.current.dt);
+          
+            this.set(
+              'weatherInfo',
+              `Current Temperature: ${temperature}°C, Time: ${currentDateTime}`
+            );
+          })
+          .catch((err) => {
+            console.error(err);
+            this.set('weatherInfo', 'Error fetching data.');
+          });
+      })
+    })
+    .catch( e => {
+      console.log(e)
+    });
+  }
 }
