db3361166fc3422c4000254e6a926168205131c0
HaoNguyen9191 <thaingoc924282@gmail.com>
2024-03-31 20:36:17 +0700

    update permission request

--- app/main-view-model.js
+++ app/main-view-model.js
@@ -1,5 +1,6 @@
 import { Observable,CoreTypes } from '@nativescript/core';
 import * as geolocation from '@nativescript/geolocation';
+const permissions = require( "@master.technology/permissions" );
 
 
 export class HomeViewModel extends Observable {
@@ -14,45 +15,53 @@ export class HomeViewModel extends Observable {
   }
 
   onGetWeather() {
-    geolocation.enableLocationRequest()
-    .then(() => {
-      geolocation.getCurrentLocation({ 
-        desiredAccuracy: CoreTypes.Accuracy.high,
-        maximumAge: 5000, 
-        timeout: 20000 })
-      .then((data)=>{
-        return {latitude: data.latitude,longitude: data.longitude}
-      })
-      .then(location=>{
-
-        const lat = location.latitude; // Latitude
-        const lon = location.longitude	; // Longitude
-        const apiKey = '672346e591b20d5d0b1e99dc21df440e'; // API Key
-        
-        fetch(
-          `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            // Example: Extracting current temperature
-            const temperature = data.current
-              ? (data.current.temp - 273.15).toFixed(2)
-              : 'N/A';
-            const currentDateTime = this.convertUnixTimestamp(data.current.dt);
+    permissions.requestPermission(permissions.PERMISSIONS.LOCATION)
+    .then( () => {
+      geolocation.enableLocationRequest()
+      .then(() => {
+        geolocation.getCurrentLocation({ 
+          desiredAccuracy: CoreTypes.Accuracy.high,
+          maximumAge: 5000, 
+          timeout: 20000 })
+        .then((data)=>{
+          return {latitude: data.latitude,longitude: data.longitude}
+        })
+        .then(location=>{
+  
+          const lat = location.latitude; // Latitude
+          const lon = location.longitude	; // Longitude
+          const apiKey = '672346e591b20d5d0b1e99dc21df440e'; // API Key
           
-            this.set(
-              'weatherInfo',
-              `Current Temperature: ${temperature}°C, Time: ${currentDateTime}`
-            );
-          })
-          .catch((err) => {
-            console.error(err);
-            this.set('weatherInfo', 'Error fetching data.');
-          });
+          fetch(
+            `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+          )
+            .then((response) => response.json())
+            .then((data) => {
+              // Example: Extracting current temperature
+              const temperature = data.current
+                ? (data.current.temp - 273.15).toFixed(2)
+                : 'N/A';
+              const currentDateTime = this.convertUnixTimestamp(data.current.dt);
+            
+              this.set(
+                'weatherInfo',
+                `Current Temperature: ${temperature}°C, Time: ${currentDateTime}`
+              );
+            })
+            .catch((err) => {
+              console.error(err);
+              this.set('weatherInfo', 'Error fetching data.');
+            });
+        })
       })
+      .catch( e => {
+        console.log(e)
+      });
     })
-    .catch( e => {
-      console.log(e)
+    .catch( () => {
+      this.set('weatherInfo', 'No Permission granted');
     });
+
+    
   }
 }
