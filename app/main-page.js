40ea1b09619e1e3972359990ede0b93cb9335050
Krystal0212 <ntu4318@gmail.com>
2024-04-06 21:19:57 +0700

    Created Basic Navigation Bar

--- app/main-page.js
+++ app/main-page.js
@@ -4,3 +4,13 @@ export function onNavigatingTo(args) {
   const page = args.object;
   page.bindingContext = createViewModel();
 }
+
+export function onLocationCheckTap(args) {
+  const page = args.object.page;
+  page.bindingContext.set("alignment", "left");
+}
+
+export function onSettingsTap(args) {
+  const page = args.object.page;
+  page.bindingContext.set("alignment", "right");
+}
